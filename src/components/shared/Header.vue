daaf3b965d3bb4137ed0d3f2d95af81641043e39
Chavez Harris <codedbychavez@gmail>
2022-08-04 18:09:05 -0300

    Added UI updates to settings modal

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,21 +1,34 @@
 <script>
 import SettingsIcon from "../icons/SettingsIcon.vue";
+import SettingsModal from "../modals/SettingsModal.vue";
 export default {
+  data() {
+    return {
+      showSettingsModal: false,
+    }
+  },
   components: {
     SettingsIcon,
+    SettingsModal,
   },
+  methods: {
+    handleClose() {
+      this.showSettingsModal = false;
+    }
+  }
 };
 </script>
 
 <template>
   <div>
     <div class="header p-4 flex">
-      <button class="p-0 text-gray-200">
+      <button @click="showSettingsModal = true" class="p-0 text-gray-200">
         <SettingsIcon />
       </button>
     </div>
 
     <!-- Modal -->
+    <SettingsModal :show="showSettingsModal" @closeSettingsModal="handleClose" />
   </div>
 </template>
 
